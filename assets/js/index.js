32809fd7203344a1caebaf0c87b846e052fc2161
Pavel <elena71vd@gmail.com>
2020-12-30 17:01:14 +0200

    do second task

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,19 +1,55 @@
 'use srtict';
 
-const arr = [4, -2, 5, 19, -130, 0, 10];
+
+//Даны числа 4, -2, 5, 19, -130, 0, 10. Найдите минимальное и максимальное число.
+
+const arr1 = [4, -2, 5, 19, -130, 0, 10];
 
 function getMinAndMax() {
-  let maxNum = arr[0];
-  let minNum = arr[0];
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] >= maxNum) {
-      maxNum = arr[i];
+  let maxNum = arr1[0];
+  let minNum = arr1[0];
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] >= maxNum) {
+      maxNum = arr1[i];
     };
-    if (arr[i] <= minNum) {
-      minNum = arr[i];
+    if (arr1[i] <= minNum) {
+      minNum = arr1[i];
     };
   };
   return [maxNum, minNum];
 };
 
 console.log(getMinAndMax());
+
+
+
+//Дан массив arr. Найдите среднее арифметическое его элементов. 
+//Проверьте задачу на массиве с элементами 12, 15, 20, 25, 59, 79.
+
+const arr2 = [12, 15, 20, 25, 59, 79];
+
+function getAverageValue() {
+  let sum = 0;
+  for (let i = 0; i < arr2.length; i++) {
+    sum += arr2[i];
+  };
+  const averageValue = sum / arr2.length;
+  return averageValue;
+};
+
+console.log(getAverageValue());
+
+
+//=========(Math.random)==========
+//Выведите на экран случайное целое число от 1 до 100.
+
+function getRandomNum() {
+  randomNum = Math.random;
+  return randomNum;
+};
+
+console.log(getRandomNum());
+
+
+//Заполните массив 10-ю случайными целыми числами. 
+
